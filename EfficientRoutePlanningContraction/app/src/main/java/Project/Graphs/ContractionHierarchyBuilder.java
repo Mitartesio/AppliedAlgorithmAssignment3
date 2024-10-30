3b3eb4242bc56e3e2b893bbf30ff01aa4fa59137
frek <frek@itu.dk>
2024-10-30 08:45:16 +0100

    ContractionHierarchyBuilder

---
+++ EfficientRoutePlanningContraction/app/src/main/java/Project/Graphs/ContractionHierarchyBuilder.java
@@ -0,0 +1,16 @@
+package Project.Graphs;
+
+public class ContractionHierarchyBuilder {
+
+    public ContractionHierarchyBuilder(GraphBuilderResult graph){
+        
+    }
+    
+
+
+    private void initializeFileOutput(){
+        String relativePath = "EfficientRoutePlanningContraction/app/src/main/resources/contractedDenmark.graph";
+    }
+
+
+}

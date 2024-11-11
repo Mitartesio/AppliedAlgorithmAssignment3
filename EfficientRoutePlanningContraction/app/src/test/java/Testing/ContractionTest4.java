0a50ffdebb7a8bb8706a23ee3aebff0bfc16ea7e
AndreasVinkler <145438734+AndreasVinkler@users.noreply.github.com>
2024-11-11 17:51:58 +0100

    Tests for local dijkstra4 added

---
+++ EfficientRoutePlanningContraction/app/src/test/java/Testing/ContractionTest4.java
@@ -0,0 +1,68 @@
+package Testing;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import Project.Dijkstra.LocalDijkstra4;
+import Project.Graphs.GraphBuilder;
+import Project.Graphs.GraphBuilderResult;
+
+public class ContractionTest4{
+
+    private static GraphBuilderResult smallGraph;
+    // private static LocalDijkstra2 ld;
+
+    @BeforeClass // Use @BeforeClass if you want to initialize the graph once for all tests
+    public static void graphInit() throws FileNotFoundException {
+        InputStream inputStream = SmallGraphTesting.class.getResourceAsStream("/Small_graph_for_test.graph");
+        if (inputStream == null) {
+            throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
+        }
+        smallGraph = GraphBuilder.buildGraphFromInputStream(inputStream);
+    }
+    
+    @Test
+    public void testZero(){
+        LocalDijkstra4 ld = new LocalDijkstra4(smallGraph.getGraph());
+        assertEquals(-2,ld.computeEdgeDifference(14));
+    }
+
+    @Test
+    public void test(){
+        LocalDijkstra4 ld = new LocalDijkstra4(smallGraph.getGraph());
+        assertEquals(-1,ld.computeEdgeDifference(16));
+    }
+
+
+@Test
+public void testPositive(){
+        LocalDijkstra4 ld = new LocalDijkstra4(smallGraph.getGraph());
+        assertEquals(1,ld.computeEdgeDifference(1));
+}
+
+@Test
+public void zeroMinusOne(){
+    LocalDijkstra4 ld = new LocalDijkstra4(smallGraph.getGraph());
+        assertEquals(-1,ld.computeEdgeDifference(4));
+}
+
+@Test
+public void testZeroTwo(){
+    LocalDijkstra4 ld = new LocalDijkstra4(smallGraph.getGraph());
+        assertEquals(0,ld.computeEdgeDifference(13));
+}
+
+@Test
+public void testMultipleCalls(){
+    LocalDijkstra4 ld = new LocalDijkstra4(smallGraph.getGraph());
+        assertEquals(-2,ld.computeEdgeDifference(14));
+        assertEquals(-1,ld.computeEdgeDifference(16));
+        assertEquals(1,ld.computeEdgeDifference(1));
+        assertEquals(-1,ld.computeEdgeDifference(4));
+        assertEquals(0,ld.computeEdgeDifference(13));
+}
+}

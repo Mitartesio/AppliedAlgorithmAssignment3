1bcd28774daa213ed979773291027ccdaa0793e6
Mitartesio <tobiaslysdal@gmail.com>
2024-11-11 16:50:38 +0100

    changing input loading in Main, adding args[i] with run {} having multiple arguments. Runnable via ./gradlew run --args='Dijkstra' etc. 'Test' works

--- EfficientRoutePlanningContraction/app/src/main/java/Project/Main/Main.java
+++ EfficientRoutePlanningContraction/app/src/main/java/Project/Main/Main.java
@@ -13,102 +13,87 @@ import Project.Graphs.GraphBuilderResult;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        InputStream inputStreamTest = Main.class.getResourceAsStream("/Small_graph_for_test.graph");
-            if (inputStreamTest == null) {
-                throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
+        // Make sure there are enough arguments before accessing them
+        if (args.length == 0) {
+            System.out.println("No arguments provided. Please specify 'Test', 'Dijkstra', or 'BiDijkstra'.");
+            return; // Exit gracefully
+        }
+
+        // Example of handling different arguments
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("Test")) {
+                System.out.println("Running Test...");
+                // Load the graph from the resource
+                InputStream inputStreamTest = Main.class.getResourceAsStream("/Small_graph_for_test.graph");
+                if (inputStreamTest == null) {
+                    throw new FileNotFoundException("Resource 'Small_graph_for_test.graph' not found in classpath");
+                }
+                GraphBuilderResult graphResultTest = GraphBuilder.buildGraphFromInputStream(inputStreamTest);
+
+                // Test the graph using the existing logic
+                long start = System.nanoTime();
+                LocalDijkstra3 ld = new LocalDijkstra3(graphResultTest.getGraph());
+                ld.computeEdgeDifference(13);
+                long end = System.nanoTime();
+                System.out.println((end - start) / 1_000_000_000.0);
+                break; // If this argument was found, stop further checks
+
+            } else if (args[i].equals("Dijkstra")) {
+                System.out.println("Running Dijkstra...");
+                // Logic for 'Dijkstra'
+                InputStream inputStreamDijkstra = Main.class.getResourceAsStream("/denmark.graph"); //
+                if (inputStreamDijkstra == null) {
+                    throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
+                }
+                GraphBuilderResult graphResultDijkstra = GraphBuilder.buildGraphFromInputStream(inputStreamDijkstra);
+
+                try (Scanner scanner = new Scanner(System.in)) {
+                    // Your code to read the start and end nodes from System.in
+                    if (scanner.hasNextInt()) {
+                        int startNode = scanner.nextInt();
+                        if (scanner.hasNextInt()) {
+                            int endNode = scanner.nextInt();
+                            // Proceed with Dijkstra algorithm
+                        } else {
+                            System.out.println("Error: End node input is missing or invalid.");
+                        }
+                    } else {
+                        System.out.println("Error: Start node input is missing or invalid.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: Invalid input format. Please enter two integer node values.");
+                }
+                break; // If this argument was found, stop further checks
+
+            } else if (args[i].equals("BiDijkstra")) {
+                System.out.println("Running BiDijkstra...");
+                // Logic for 'BiDijkstra'
+                InputStream inputStreamBiDijkstra = Main.class.getResourceAsStream("/denmark.graph");
+                if (inputStreamBiDijkstra == null) {
+                    throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
+                }
+                GraphBuilderResult graphResultBiDijkstra = GraphBuilder
+                        .buildGraphFromInputStream(inputStreamBiDijkstra);
+
+                Scanner scanner = new Scanner(System.in);
+                for (int j = 0; j < 1000; j++) {
+                    BidirectionalDijkstra biDijkstra = new BidirectionalDijkstra(graphResultBiDijkstra.getGraph());
+
+                    int startNode = scanner.nextInt();
+                    int endNode = scanner.nextInt();
+
+                    long startTime = System.nanoTime();
+                    biDijkstra.computeShortestPath(startNode, endNode);
+                    long endTime = System.nanoTime();
+
+                    float totalTime = (endTime - startTime) / 1_000_000_000.0f;
+                    System.out.println("BiDijkstra " + biDijkstra.getCounterRelaxed() + " " + totalTime);
+                }
+                scanner.close();
+                break; // If this argument was found, stop further checks
+            } else {
+                System.out.println("Unknown argument: " + args[i]);
             }
-            GraphBuilderResult graphResultTest = GraphBuilder.buildGraphFromInputStream(inputStreamTest);
-            
-            long start = System.nanoTime();
-            LocalDijkstra3 ld = new LocalDijkstra3(graphResultTest.getGraph());
-            ld.computeEdgeDifference(13);
-            // ContractionHierarchy cont = new ContractionHierarchy(graphResultTest.getGraph());
-            long end = System.nanoTime();
-            System.out.println((end - start) / 1_000_000_000.0);
-            
-        if(args[0].equals("Test")){
-            InputStream inputStream = Main.class.getResourceAsStream("/denmark.graph");
-            if (inputStream == null) {
-                throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
-            }
-            GraphBuilderResult graphResult = GraphBuilder.buildGraphFromInputStream(inputStream);
-            
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("nu");
-            int startNode = scanner.nextInt();
-            int endNode = scanner.nextInt();
-            DijkstraUndirectedSP dijkstra = new DijkstraUndirectedSP(graphResult.getGraph());
-            dijkstra.computeShortestPath(startNode,endNode);
-            System.out.println("Test done");
-            scanner.close();
         }
-
-        if(args[0].equals("Dijkstra")){
-            InputStream inputStream = Main.class.getResourceAsStream("/denmark.graph");
-        if (inputStream == null) {
-            throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
-        }
-        GraphBuilderResult graphResult = GraphBuilder.buildGraphFromInputStream(inputStream);
-        //Take start- and endnode
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i<1000; i++){
-            int startNode = scanner.nextInt();
-            int endNode = scanner.nextInt();
-            //start nanoTime
-            long startTime = System.nanoTime();
-            
-            //Make graph with start and end node
-            DijkstraUndirectedSP dijkstra = new DijkstraUndirectedSP(graphResult.getGraph());
-            dijkstra.computeShortestPath(startNode,endNode);
-            //end nanotime
-            long endTime = System.nanoTime();
-
-            float totalTime = (endTime-startTime)/1_000_000_000.0f;
-            System.out.println("Djikstra " + dijkstra.getCounterRelaxed() + " " + totalTime);
-        }
-        scanner.close();
-        }
-        if(args[0].equals("BiDijkstra")){
-            InputStream inputStream = Main.class.getResourceAsStream("/denmark.graph");
-        if (inputStream == null) {
-            throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
-        }
-        GraphBuilderResult graphResult = GraphBuilder.buildGraphFromInputStream(inputStream);
-
-
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i<1000; i++){
-            BidirectionalDijkstra biDjikstra = new BidirectionalDijkstra(graphResult.getGraph());
-
-            int startNode = scanner.nextInt();
-            int endNode = scanner.nextInt();
-
-            long startTimeSecond = System.nanoTime();
-
-            biDjikstra.computeShortestPath(startNode, endNode);
-
-            long endTimeSecond = System.nanoTime();
-
-            float totalTimeSecond = (endTimeSecond-startTimeSecond)/1_000_000_000.0f;
-
-            System.out.println("DjikstraBirectional " + biDjikstra.getCounterRelaxed() + " " + totalTimeSecond);
-        }
-        scanner.close();
     }
-
-        // String filePath = "Efficient Route Planning/app/src/main/resources/denmark.graph";
-        // GraphBuilderResult result = GraphBuilder.buildGraphFromFile(filePath);
-        // InputStream inputStream = Main.class.getResourceAsStream("/denmark.graph");
-        // if (inputStream == null) {
-        //     throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
-        // }
-        // GraphBuilderResult graphResult = GraphBuilder.buildGraphFromInputStream(inputStream);
-        // System.out.println(graphResult.getLongIntegerMap().size());
-        // int startNode = 10;
-        // int endNode = 20;
-        // DijkstraUndirectedSP dijkstra = new DijkstraUndirectedSP(graphResult.getGraph(),startNode,endNode);
-        // System.out.println(dijkstra.getCounterRelaxed());
-    }
-
-
 }
